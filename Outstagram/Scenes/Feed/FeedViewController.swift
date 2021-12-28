f32a0e13674389d2e9383b7053273c165fcd018a
Yoo MinSeo <msyou999@gmail.com>
2021-12-28 15:20:18 +0900

    feat : 게시글 업로드 및 사진 접근권한

--- Outstagram/Scenes/Feed/FeedViewController.swift
+++ Outstagram/Scenes/Feed/FeedViewController.swift
@@ -19,6 +19,16 @@ class FeedViewController: UIViewController {
         
         return tableView
     }()
+    
+    private lazy var imagePickerController: UIImagePickerController = {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        
+        return imagePickerController
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +53,28 @@ extension FeedViewController: UITableViewDataSource {
     }
 }
 
+extension FeedViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var selectImage: UIImage?
+        
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            selectImage = editedImage
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            selectImage = originalImage
+        }
+        
+        print(selectImage)
+        
+        picker.dismiss(animated: true) { [weak self] in
+            let uploadViewController = UploadViewController(uploadImage: selectImage ?? UIImage())
+            let navigationController = UINavigationController(rootViewController: uploadViewController)
+            navigationController.modalPresentationStyle = .fullScreen
+            
+            self?.present(navigationController, animated: true)
+        }
+    }
+}
+
 private extension FeedViewController {
     func setupNavigationBar() {
         navigationItem.title = "Outstagram"
@@ -51,9 +83,13 @@ private extension FeedViewController {
             image: UIImage(systemName: "plus.app"),
             style: .plain,
             target: self,
-            action: nil
+            action: #selector(didTabUploadButton)
         )
         navigationItem.rightBarButtonItem = uploadButton
+    }
+    
+    @objc func didTabUploadButton() {
+        present(imagePickerController, animated: true)
     }
     
     func setupTableView() {
